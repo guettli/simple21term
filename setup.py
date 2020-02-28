2f9c655899653888686da967c42a36e9cbd9b78e
Thomas Güttler <guettliml@thomas-guettler.de>
2020-02-28 20:56:43 +0100

    setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='whatandwhotree',
+      version='1.0',
+      description='What and who tree',
+      author='Thomas Güttler',
+      author_email='guettli.whatandwhotree@thomas-guettler.de',
+      url='https://github.com/guettli/whatandwhotree/',
+      packages=['whatandwhotree'],
+     )
