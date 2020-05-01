5ccf405dc101abf18a1a952de8c97fa9431da8ed
Thomas Guettler <info.git@thomas-guettler.de>
2020-05-01 20:07:36 +0200

    .

--- mysite/settings.py
+++ mysite/settings.py
@@ -12,7 +12,7 @@ SECRET_KEY = 'h!mhp442)oiqc=&uhlny@_@6n!66-aypl^4dk)vwki=yxqs2#*'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['testserver']
+ALLOWED_HOSTS = ['testserver', 'localhost']
 
 # Application definition
 
